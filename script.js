256dea6f2e05088bbc2b29676ea89e06aabc0cd8
Osondu Gabriel <136438699+OsonduGabriel@users.noreply.github.com>
2026-02-14 12:50:10 +0100

    worked on download and hire button

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const backToTop = document.querySelector(".back_to_top")
+const cvBtn = document.querySelector(".btn_cv")
+const hireBtn = document.querySelector(".btn_hire")
+const modal = document.getElementById("hireModal") //modal overlay ID
+const closeBtn = document.querySelector(".close_modal")
+const hireForm = document.querySelector(".hireForm")
 
+//========= Back to Top ============
+//backToTop button should be visible when scrolled up to window Y 500px
 window.addEventListener("scroll", () => {
     if(window.scrollY > 500){
         backToTop.style.opacity = "1"
@@ -7,5 +14,43 @@ window.addEventListener("scroll", () => {
     } else{
         backToTop.style.opacity = "0"
         backToTop.style.visibility = "hidden"
+    }
+})
+
+// ======== Download CV =========
+cvBtn.addEventListener("click", () => {
+    window.location.href = "assets/Gabriel_CV.pdf"  //download cv
+})
+
+
+//========= Modal pop-up window ===========
+//open modal  when hire me button is clicked
+
+if(hireBtn){
+    hireBtn.addEventListener("click", () => {
+        modal.classList.add("active") //open modal by attaching class active which was created in css
+        document.body.style.overflow = "hidden" //prevent scrolling of body
+    })
+}
+
+//close modal function
+function closeModal(){
+    modal.classList.remove("active") //remove class active
+    document.body.style.overflow = ''
+}
+
+closeBtn.addEventListener("click", closeModal) //close modal with button
+
+//click outside content to close modal
+modal.addEventListener("click", (event) => {
+    if(event.target === modal){
+        closeModal()
+    }
+})
+
+//ESC key closes modal
+document.addEventListener("keydown", (event) => {
+    if(event.key === "Escape"){
+        closeModal()
     }
 })
